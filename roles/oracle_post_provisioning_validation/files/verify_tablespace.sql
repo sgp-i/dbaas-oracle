53c3cbe4d604e6339bd8cd0ab52fd6bfe3cb9c0a
Harpreet singh <harpreet@xenonstack.com>
2019-01-15 18:07:22 +0530

    added exit to sql files

--- roles/oracle_post_provisioning_validation/files/verify_tablespace.sql
+++ roles/oracle_post_provisioning_validation/files/verify_tablespace.sql
@@ -7,3 +7,4 @@ Col bytes format 9999999.999
 Col maxbytes format 9999999.999
 select tablespace_name,file_name, autoextensible, bytes/1024/1024/1024 bytes, maxbytes/1024/1024/1024 maxbytes from dba_data_files order by file_id;
 spool off
+EXIT
